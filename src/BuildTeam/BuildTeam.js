27d1f5c5eac2fda2d8e4d57f92684fa2fe48613e
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2020-02-27 03:14:18 +0000

    team build functional

--- src/BuildTeam/BuildTeam.js
+++ src/BuildTeam/BuildTeam.js
@@ -13,13 +13,14 @@ export class BuildTeam extends Component {
     e.preventDefault();
     const { addTeam } = this.context;
     const { name } = e.target;
-    const { slots } = [0, 1, 2, 3, 4, 5].forEach(
-      i => e.target[`pokemon-${i}`].value
-    );
+    const slots = [1, 2, 3, 4, 5, 6].map(i => e.target[`pokemon-${i}`].value);
+
     const team = {
       name: name.value,
-      slots: slots.value
+      slots: slots
     };
+
+    console.log(team);
 
     addTeam(team);
   };
@@ -49,51 +50,36 @@ export class BuildTeam extends Component {
                 ))}
               </select>
 
-              <select name='pokemon-2'>
-                <option disabled selected value>
-                  {' '}
-                  -- Slot 2 --{' '}
-                </option>
+              <select defaultValue='' name='pokemon-2'>
+                <option value=''> -- Slot 2 -- </option>
                 {pokemon.map(pokemon => (
                   <option value={pokemon.name}>{pokemon.name}</option>
                 ))}
               </select>
 
-              <select name='pokemon-3'>
-                <option disabled selected value>
-                  {' '}
-                  -- Slot 3 --{' '}
-                </option>
+              <select defaultValue='' name='pokemon-3'>
+                <option value=''> -- Slot 3 -- </option>
                 {pokemon.map(pokemon => (
                   <option value={pokemon.name}>{pokemon.name}</option>
                 ))}
               </select>
 
-              <select name='pokemon-4'>
-                <option disabled selected value>
-                  {' '}
-                  -- Slot 4 --{' '}
-                </option>
+              <select defaultValue='' name='pokemon-4'>
+                <option value=''> -- Slot 4 -- </option>
                 {pokemon.map(pokemon => (
                   <option value={pokemon.name}>{pokemon.name}</option>
                 ))}
               </select>
 
-              <select name='pokemon-5'>
-                <option disabled selected value>
-                  {' '}
-                  -- Slot 5 --{' '}
-                </option>
+              <select defaultValue='' name='pokemon-5'>
+                <option value=''> -- Slot 5 -- </option>
                 {pokemon.map(pokemon => (
                   <option value={pokemon.name}>{pokemon.name}</option>
                 ))}
               </select>
 
-              <select name='pokemon-6'>
-                <option disabled selected value>
-                  {' '}
-                  -- Slot 6 --{' '}
-                </option>
+              <select defaultValue='' name='pokemon-6'>
+                <option value=''> -- Slot 6 -- </option>
                 {pokemon.map(pokemon => (
                   <option value={pokemon.name}>{pokemon.name}</option>
                 ))}
